5765e077d1504e2a1154ea2974c5cbab404945f2
Ryan Su <suxi@oregonstate.edu>
2021-05-28 16:35:13 -0700

    fixed test file spacing

--- test_calculator.py
+++ test_calculator.py
@@ -3,7 +3,7 @@ import calculator
 
 class TestCalculatorApp:
     def test_add(self):
-        assert 10 == calculator.add(5,5)
+        assert 10 == calculator.add(5, 5)
 
     def test_subtract(self):
-        assert 10 == calculator.subtract(15,5)
+        assert 10 == calculator.subtract(15, 5)
