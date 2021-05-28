9c8a699541273674a452951dad18a53defde2d58
Ryan Su <suxi@oregonstate.edu>
2021-05-28 16:37:30 -0700

    wrote test for multiply and divide

--- test_calculator.py
+++ test_calculator.py
@@ -7,3 +7,9 @@ class TestCalculatorApp:
 
     def test_subtract(self):
         assert 10 == calculator.subtract(15, 5)
+
+    def test_multiply(self):
+        assert 25 == calculator.multiply(5, 5)
+
+    def test_divide(self):
+        assert 5 == calculator.divide(25, 5)
